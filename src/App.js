2e05157152d39988c5f3dd073d10c893d80d4410
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-06-20 00:29:25 +0500

    // made basic UI and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header';
+import './App.css'
+import Total from './components/total';
+import History from './components/history';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mn">
+      <div className="main">
+      <Header></Header>
+      <Total></Total>
+      <History></History>
+      <AddTransaction></AddTransaction>
     </div>
+    </div>
+    
   );
 }
 
