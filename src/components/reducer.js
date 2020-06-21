6d0aec994b51e06ed177c814fd81d907c39c35d2
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-06-21 15:06:15 +0500

    // some fixing for removing transaction

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -4,8 +4,8 @@ export const transReducer = (state,action)=>{
         case "add":
             return [action.data, ...state]
         case "remove":
-            return state.splice(action.delIndex,1);
+            return [...state.slice(0, action.delIndex),...state.slice(action.delIndex+1)]
         default:
-            return state;
+            return state
     }
 }
